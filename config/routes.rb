abc7f83c4ac58bc5dce89072c608b0e94c9443ba
anaresgalla <anaresgalla@gmail.com>
2025-02-05 11:45:05 -0300

    Adds Admin controller show route and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admins
   resources :products
+  resource :admin, only: [ :show ], controller: :admin
 
   get "up" => "rails/health#show", as: :rails_health_check
 
