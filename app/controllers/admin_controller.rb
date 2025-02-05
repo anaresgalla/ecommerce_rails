482e30626c6f57b02c0e6c557e48453e9d3ac623
anaresgalla <anaresgalla@gmail.com>
2025-02-05 15:59:23 -0300

    Improves secure access

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,11 +3,4 @@ class AdminController < ApplicationController
 
   def show
   end
-
-  private
-  def check_admin_privileges
-    if !current_admin
-      redirect_to root_path
-    end
-  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  protected
+
+  def check_admin_privileges
+    if !current_admin
+      redirect_to root_path
+    end
+  end
 end
