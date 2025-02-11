a5e58f5656658907d5fa94b2efff4e453e692899
anaresgalla <anaresgalla@gmail.com>
2025-02-11 14:53:46 -0300

    Adds supported shipping countries and address

--- app/controllers/buy_now_controller.rb
+++ app/controllers/buy_now_controller.rb
@@ -16,6 +16,10 @@ class BuyNowController < ApplicationController
           },
           quantity: 1
         } ],
+        shipping_address_collection: {
+          allowed_countries: STRIPE_SUPPORTED_COUNTRIES
+        },
+
         mode: "payment",
         return_url: success_product_buy_now_url(@product)
     })
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,9 @@
-Stripe.api_key = Rails.application. credentials.dig(:stripe, :sk)
+Stripe.api_key = Rails.application.credentials.dig(:stripe, :sk)
+STRIPE_SUPPORTED_COUNTRIES = [ "AR", "AU", "AT", "BE", "BO", "BR", "BG", "CA",
+                              "CL", "CO", "CR", "HR", "CY", "CZ", "DK", "DO",
+                              "EG", "EE", "FI", "FR", "DE", "GR", "HK", "HU",
+                              "IS", "IN", "ID", "IE", "IL", "IT", "JP", "LV",
+                              "LI", "LT", "LU", "MT", "MX", "NL", "NZ", "NO",
+                              "PY", "PE", "PL", "PT", "RO", "SG", "SK", "SI",
+                              "ES", "SE", "CH", "TH", "TT", "AE", "GB", "US", "UY"
+                              ].freeze
