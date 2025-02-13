39bfddde7c0a05e641af9ed0085cf6785b7c7ac3
anaresgalla <anaresgalla@gmail.com>
2025-02-13 13:55:10 -0300

    Create add to cart function

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,18 @@
 class CartsController < ApplicationController
+  before_action :set_current_cart
+
   def create
+    @current_cart.cart_items.create(product_id: params[:product_id])
+  end
+
+  private
+
+  def set_current_cart
+    if session[:current_cart_id]
+      @current_cart = Cart.find_by_secret_id(session[:current_cart_id])
+    else
+      @current_cart = Cart.create
+      session[:current_cart_id] = @current_cart.secret_id
+    end
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,12 @@
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items
+
+  before_create :set_secret_id
+
+  private
+
+  def set_secret_id
+    self.secret_id = SecureRandom.uuid + DateTime.now.to_i.to_s # enhance security by adding timestamp
+  end
 end
