4ded2af16a844d00c7629d2deda882b69773fb1d
anaresgalla <anaresgalla@gmail.com>
2025-02-14 12:53:51 -0300

    Adds cart statuses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_13_150533) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_14_151842) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -77,6 +77,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_13_150533) do
     t.string "secret_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
   end
 
   create_table "categories", force: :cascade do |t|
